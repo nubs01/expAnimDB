1f4180a39201524395ae27500d789ccc25e7c44d
Roshan Nanu <rnanu@brandeis.edu>
2018-06-24 15:05:58 -0400

    Make preprocess functions using only the animal DB and animID

---
+++ getPreprocessData.m
@@ -0,0 +1,107 @@
+function outData = getPreprocessData(animID,dbPath)
+    % Searches animal metadata database and gathers info needed for
+    % preprocessing such as target area for tetrodes, references and diodenum
+
+    % load animal database
+    if ~exist('dbPath','var')
+        dbPath = '~/Projects/rn_Schizophrenia_Project/animal_metadata.mat';
+    end
+    animDB = load(dbPath);
+    animDB = animDB.animDB;
+
+    % find animal
+    animIdx = find(strcmpi({animDB.animal},animID));
+    if isempty(animIdx)
+        disp('Animal not found in database');
+        outData = [];
+        return;
+    end
+    animDat = animDB(animIdx);
+    recDat = animDat.recording_data;
+    days = numel(recDat);
+    if days==0
+        outData = [];
+        return;
+    end
+    for l=1:days
+        tetDat(:,l) = recDat(l).tet_info'; %struct array with tetrodes as rows and days as columns
+    end
+
+    % look through tet data and collect info:
+    % - areas
+    % - tet's targeting each area
+    % - riptets
+    % - depths
+    % - EMG 
+    % - reference tetrode each day
+    % - tetrodes that should be delete (dead/LFPchan = 0 in DB)
+    % - tracking diode num (recData)
+
+    expDir = [animDat.project filesep animDat.experiment_dir];
+    if strcmp(expDir(end),filesep)
+        expDir = expDir(1:end-1);
+    end
+    animDir = [expDir filesep animID];
+
+    tetAreas = {tetDat(:,1).target};
+    areas = unique(tetAreas);
+    tetLists = cell(1,numel(areas));
+    for l=1:numel(areas)
+       tetLists{l} = find(strcmpi(tetAreas,areas{l}));
+    end 
+
+    % remove  EMG and make separate
+    emgIdx = find(strcmpi(areas,'EMG'));
+    if ~isempty(emgIdx)
+        emgList  = tetLists{emgIdx};
+        tetLists(emgIdx) = [];
+        areas(emgIdx) = [];
+    else
+        emgList = [];
+    end
+
+    % start outData structure
+    outData.animal = animID;
+    outData.expDir = expDir;
+    outData.animDir = animDir;
+    outData.areas = areas;
+    outData.tetLists = tetLists;
+    outData.emgList = emgList;
+
+    % find refs and riptets
+    % TODO: Reformatting refList to be refList{day}(area) - need to rewrite preprocess appropriately
+    refList = cell(1,days);
+    riptetlist = cell(1,days);
+    depths = zeros(size(tetDat,1),days);
+    diodenum = cell(1,days);
+    deleteTrodes = cell(1,days);
+    rawDirs = cell(1,days);
+    for l=1:days
+        rawDirs{l} = sprintf('%02i_%s',recDat(l).day,datestr(recDat(l).date,'YYmmDD'));
+        riptetlist{l} = find([tetDat(:,l).riptet]);
+        refList{l} = zeros(1,numel(areas));
+        for k=1:numel(areas)
+            tmp = find([tetDat(strcmpi(tetAreas,areas{k}),l).ref]);
+            if ~isempty(tmp)
+                refList{l}(k) = tmp;
+            end
+        end
+        for k=1:numel(tetAreas)
+            tmp = tetDat(k,l).depth;
+            if isempty(tmp)
+                depths(k,l) = nan;
+            else
+                depths(k,l) = tmp;
+            end
+        end
+        diodenum{l} = [recDat(l).epochs.diode_num]; % diodenum{day}(epoch) gives diodes to use in pos extraction, TODO: adjust preprocess
+        deleteTrodes{l} = strfind([tetDat(:,l).lfp_channel],'0');
+    end
+
+    outData.rawDirs = rawDirs;
+    outData.refList = refList;
+    outData.riptetlist = riptetlist;
+    outData.depths = depths;
+    outData.diodenum = diodenum;
+    outData.deleteTrodes = deleteTrodes; % tetrodes to delete because they are dead and were not removed from recording
+end
---
+++ rn_preprocessAnimal.m
@@ -0,0 +1,200 @@
+function out = rn_preprocessAnimal(animID,varargin)
+    % Get animal data from default metadata database or given dbPath and
+    % preprocess for SZ delta analysis. Requires position tracking be done and
+    % that the diodenum in the database is what was used for tracking. the
+    % flags doDIO and doSpikes tell it whether to preprocess spikes and dio. 
+    % NAME - VALUE Pairs
+    %   projDir :   path to project directory on computer i.e. '/data/Projects/'
+    %   dbPath  :   path to animal metadata database
+    %   doDIO   :   flag for whether to create DIO files
+    %   doSpikes:   flag for whether to create Spikes files
+    %   dioPulseFrameChannel : dio channel of camera strobe (leave empty if no strobe)
+    %   min_suprathresh_duration : min time for ripple power to be above threshold to count as a ripple (seconds)
+    %   nstd    :   ripple power threshold, number of std above baseline
+    %   daysToAnalyze : list of days to analyze, leave empty to analyze all days for animal
+    %   nTets   : maximum number of tetrodes on headstage (default=8)
+
+    out = [];
+    projDir = '~/Projects/';
+    dbPath = [projDir 'rn_Schizophrenia_Project/animal_metadata.mat'];
+    doDIO = 0; % create DIO files
+    doSpikes = 0; % create spikes files; leave as 0 if unclustered
+
+    dioPulseFrameChannel = []; % dio channel of camera strobe pulse, empty if no strobe
+
+    %% Variables for ripple detection/extraction
+    min_suprathresh_duration = 0.015; % min time for ripple power to be above threshold to count as a ripple (in seconds)
+    nstd = 3; % ripple power threshold: number of standard deviations above baseline
+
+    daysToAnalyze = []; % will analyze all days if empty
+    nTets = 8;
+
+    assignVars(varargin)
+
+    % Grab & Confirm directory information
+    animInfo = getPreprocessData(animID,dbPath);
+    animDir = [projDir animInfo.animDir filesep];
+    dataDir = [projDir animInfo.expDir filesep animID '_direct' filesep];
+    currDir = pwd;
+    logFile = [dataDir animID '_preprocess.log'];
+    rawDirs = strcat(animDir,animInfo.rawDirs');
+    dirCheck = cellfun(@(x) exist(x,'dir'),rawDirs);
+    if ~all(dirCheck)
+        disp('ERROR: Cannot find raw data directories:')
+        cellfun(@(x) disp(['      ' x]),rawDirs(find(dirCheck)))
+        out = -1;
+        return;
+    end
+
+    % Create convenient helper variables
+    if isempty(daysToAnalyze)
+        daysToAnalyze = linspace(1,numel(rawDirs),numel(rawDirs));
+    end
+    
+
+    %% Start log
+    if exist(logFile,'file')
+        HEADER = 1;
+    else
+        HEADER = 0;
+    end
+%    diary(logFile)
+    if HEADER
+        fprintf('\n\n')
+    end
+    disp(datestr(now,0))
+    a = sprintf('%g ',daysToAnalyze);
+    % Data check output for user
+    disp(['Preparing to preprocess ' animID])
+    disp(['    animal directory: ' animDir])
+    disp('    day directories: ')
+    cellfun(@(x) disp(['        ' x]),animInfo.rawDirs)
+    disp(['    output to: ' dataDir])
+    disp(['    log: ' logFile])
+    disp(['    Analyzing Days : ' a])
+    
+    q = input('Is this ok? (y/n)','s');
+    if strcmpi(q,'n')
+        return;
+    end
+    fprintf('\n\n')
+
+    %% Process each day
+    for sessionNum = daysToAnalyze
+        fprintf('Processing day %02i....\n',sessionNum)
+        
+        dayNumStr = sprintf('%02i',sessionNum);
+        dayDir = rawDirs{sessionNum};
+
+
+        % Extract Binaries to matlab files
+        %rn_createNQPosFiles(dayDir,dataDir,animID,sessionNum,[],'diodenum',animInfo.diodenum{sessionNum})
+        rn_createNQLFPFiles(dayDir,dataDir,animID,sessionNum)
+        if doDIO
+            mcz_createNQDIOFiles(dayDir,dataDir,animID,sessionNum)
+            % createNQDIOFilesFromStateScriptLogs(rawDir, dataDir, animID, sessionNum) % if DIO needs to be extracted from statescript logs
+        end
+        if doSpikes
+            rn_createNQSpikesFiles(dayDir,dataDir,animID,sessionNum)
+        end
+
+        % Delete dead tetrodes
+        delTrodes = animInfo.deleteTrodes{sessionNum};
+        if ~isempty(delTrodes)
+            fprintf('    Deleting dead tetrodes: %s\n',num2str(animInfo.deleteTrodes{sessionNum}))
+            eegDir = [dataDir 'EEG' filesep];
+            for l=delTrodes
+                delFiles = dir(sprintf('%s%s*-*-%02i.mat',eegDir,animID,l));
+                delFiles = {delFiles.name};
+                delFiles = strcat(eegDir,delFiles);
+                fprintf('       deleting %s\n',delFiles)
+                cellfun(@(x) delete(x),delFiles);
+            end
+        end
+
+        % Convert to EMG from EEG file
+        if ~isempty(animInfo.emgList)
+            emgDir = [dataDir 'EMG' filesep];
+            eegDir = [dataDir 'EEG' filesep];
+            if ~exist(emgDir,'dir')
+                mkdir(emgDir)
+            end
+            for l = animInfo.emgList
+                eegFiles = dir(sprintf('%s%s*-*-%02i.mat',eegDir,animID,l));
+                if isempty(eegFiles)
+                    continue;
+                end
+                eegFiles = {eegFiles.name};
+                emgFiles = strrep(eegFiles,'eeg','emg');
+                eegFiles = strcat(eegDir,eegFiles);
+                emgFiles = strcat(emgDir,emgFiles);
+                cellfun(@(x,y) movefile(x,y),eegFiles,emgFiles);
+            end
+        end
+
+        % Filter and Reference EEG files
+        cd(dayDir)
+        epochs = getEpochs(1);
+        cd(currDir)
+        nEpochs = size(epochs,1);
+
+        refData = zeros(nEpochs,nTets);
+        for l=1:numel(animInfo.areas)
+            for k = animInfo.tetLists{l}
+                refData(:,k) = animInfo.refList{sessionNum}(l);
+            end
+        end
+        fprintf('Creating referenced EEG files...\n')
+        mcz_createRefEEG(dayDir, dataDir, animID, sessionNum, refData)
+
+        % Create filtered eeg data files
+        filterDir = [fileparts(which('mcz_deltadayprocess.m')) filesep 'filters' filesep];
+        fprintf('Delta Filtering LFPs...\n')
+        mcz_deltadayprocess(dayDir, dataDir, animID, sessionNum, 'f', [filterDir 'deltafilter.mat']) %can add option mcz...(...,'f',...,'ref',1) if you want to filter with eegref instead of eeg (default)
+        fprintf('Gamma Filtering LFPs...\n')
+        mcz_gammadayprocess(dayDir, dataDir, animID, sessionNum, 'f', [filterDir 'gammafilter.mat']) % for gamma filter default is to use eegref, can pass 'ref',0 to filter eeg instead
+        fprintf('Ripple Filtering LFPs...\n')
+        mcz_rippledayprocess(dayDir, dataDir, animID, sessionNum, 'f', [filterDir 'ripplefilter.mat']) % for ripple filter default is to use eegref, can pass 'ref',0 to filter eeg instead
+        fprintf('Theta Filtering LFPs...\n')
+        mcz_thetadayprocess(dayDir, dataDir, animID, sessionNum, 'f', [filterDir 'thetafilter.mat']) % for theta filter default is to use eeg, can pass 'ref',1 to filter eegref instead
+
+        % extract ripples
+        fprintf('Detecting and Extracting Ripples...\n')
+        extractripples(dataDir,animID,sessionNum,animInfo.riptetlist{sessionNum},min_suprathresh_duration,nstd);
+
+        % Detect EEG artifacts using all avaible tetrodes
+        % TODO: Move before ripple extraction and use to help with ripple detection
+        fprintf('Detecting and Marking Artifacts...\n')
+        %rn_detectArtifacts(dataDIr, animID, sessionNum)
+
+        % Score behavioral states
+        % TODO: Move before ripple extraction and use to help with ripple detection
+        % TODO: Create behavioral state algorithm. Use RW6.
+        if ~isempty(animInfo.emgList)
+            fprintf('Scoring behavioral states using Position and EMG data...\n')
+        else
+            fprintf('EMG Not Available: Scoring behavioral states using only Position data...\n')
+        end
+
+        fprintf('Day %02d complete!\n\n\n',sessionNum)
+    end
+
+
+    %% Create cell and tet info metadata structures
+    disp('Creating cell & tet info structures')
+    rn_createtetinfostruct(dataDir,animID);
+    mcz_createcellinfostruct(dataDir,animID);
+
+    for day=daysToAnalyze
+        for i=1:numel(animInfo.areas)
+            rn_addtetrodelocation(dataDir,animID,animInfo.tetLists{i},animInfo.areas{i},day);
+            rn_addtetrodedescription(dataDir,animID,animInfo.refList{day}(i),[animInfo.areas{i} 'Ref'],day);
+            rn_addtetrodedescription(dataDir,animID,animInfo.riptetlist{day},'riptet',day);
+        end
+    end
+
+    sj_addcellinfotag2(dataDir,animID);
+    disp('Preprocessing complete!')
+    disp(datestr(now,0))
+    diary off
+    out = 1;
